837b69c44cecd11a73f9f9ee7113f34bf19a7d71
ShouryaSahu <yourgithubemail@gmail.com>
2026-08-11 23:24:06 +0530

    React Router

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold bg-green-400 text-center'>React Router</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+
+  const data = useLoaderData()
+  // const [data, setData] = useState([])
+  // useEffect(() => {
+  //   // Fetch Github followers data
+  //   fetch('https://api.github.com/users/shouryasahu')
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     console.log(data);
+  //     setData(data)
+  //   })
+  // }, [])
+
+  return (
+    <div className='text-center m-4 bg-gray-500 text-white text-4xl p-4'>Github Followers: {data.followers}
+    <img src={data.avatar_url} alt="Git Picture" width={200} />
+    </div>
+  )
+}
+
+export default Github
+
+export const githubInfoLoader = async () => {
+  const response = await fetch('https://api.github.com/users/shouryasahu');
+    return response.json();
+}
